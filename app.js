2bd4bc23a47861a39a04616bbe7ce63014fddd64
Tom Hennessy <tomlhennessy@outlook.com>
2024-12-17 15:54:20 +1000

    sign, decode and verifY JWT

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ require('dotenv').config();
 
 // Import package
 
-// Your code here
+const jwt = require('jsonwebtoken');
 
 // Define variables - DO NOT MODIFY
 
 // 1. Sign (create) a JWT containing your email address
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
-// Your code here
+const payloadData = { email: "tomlhennessy@outlook.com" };
+
+token = jwt.sign(
+    payloadData,
+    process.env.SECRET_KEY,
+    { expiresIn: '1h' }
+)
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -19,7 +25,7 @@ console.log('JWT:', token);
 
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
-// Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -28,7 +34,7 @@ console.log('Payload:', payload);
 
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
-// Your code here
+verifiedPayload = jwt.verify(token, process.env.SECRET_KEY);
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
@@ -38,7 +44,11 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify
 //    Then "catch" the error and log it to the console.
 
-// Your code here
+try {
+    jwt.verify(token, 'wrong-secret');
+} catch (err) {
+    console.log("Error:", err.message);
+}
 
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
@@ -46,4 +56,16 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify (with proper secret)
 //    Then "catch" the error and log it to the console
 
-// Your code here
+token = jwt.sign(
+    { email: "tomlhennessy@outlook.com"},
+    process.env.SECRET_KEY,
+    { expiresIn: '1s' }
+);
+
+setTimeout(() => {
+    try {
+        jwt.verify(token, process.env.SECRET_KEY);
+    } catch (err) {
+        console.log("Error:", err.message);
+    }
+}, 2000);
